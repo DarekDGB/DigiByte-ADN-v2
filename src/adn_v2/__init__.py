297fa7d013a69b0c21d7ba2e12b07c80c20ea80d
DarekDGB <majescyk@gmail.com>
2025-11-23 19:58:46 +0000

    Create __init__.py

---
+++ src/adn_v2/__init__.py
@@ -0,0 +1,8 @@
+"""
+DigiByte Autonomous Defense Node v2 (ADN v2)
+
+Layer-3 defense engine that executes actions based on
+Sentinel AI v2 + DQSN + Wallet Guardian signals.
+"""
+
+__version__ = "0.1.0"
