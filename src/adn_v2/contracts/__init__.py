c07c4f00af2363db9e3469f70377f9229033651a
DarekDGB <majescyk@gmail.com>
2026-01-04 22:15:05 +0000

    test(v3): enforce fail-closed behavior for invalid ADN v3 contract version

--- src/adn_v2/contracts/__init__.py
+++ src/adn_v2/contracts/__init__.py
@@ -1,1 +1,5 @@
-todo
+from .v3_reason_codes import ReasonCode
+from .v3_hash import canonical_sha256
+from .v3_types import ADNv3Request
+
+__all__ = ["ReasonCode", "canonical_sha256", "ADNv3Request"]
