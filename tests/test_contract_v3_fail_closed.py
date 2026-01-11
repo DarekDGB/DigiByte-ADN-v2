3a59542508e54d22c2ff0d394981b85aa8f97ed8
DarekDGB <majescyk@gmail.com>
2026-01-11 23:07:28 +0000

    test: point v3 fail-closed contract test to adn_v3

--- tests/test_contract_v3_fail_closed.py
+++ tests/test_contract_v3_fail_closed.py
@@ -1,8 +1,8 @@
 import pytest
 
 from adn_v2.config import ADNConfig
-from adn_v2.v3 import ADNv3
-from adn_v2.contracts.v3_reason_codes import ReasonCode
+from adn_v3 import ADNv3
+from adn_v3.contracts.v3_reason_codes import ReasonCode
 
 
 def test_contract_v3_invalid_version_fails_closed():
