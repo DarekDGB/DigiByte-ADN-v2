44012428a39cbc4ee40a353ab3d31f2af9d9a85b
DarekDGB <majescyk@gmail.com>
2026-01-11 22:25:29 +0000

    chore: scaffold adn_v3 package namespace

---
+++ src/adn_v3/__init__.py
@@ -0,0 +1,7 @@
+"""
+ADN v3 (authoritative)
+
+This package is the canonical implementation of the ADN Shield Contract v3 surface.
+
+Legacy code remains under `adn_v2` for reference/compatibility only.
+"""
